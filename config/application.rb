748a555627cdcb172a1780feaff35041da48da31
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-01 19:00:49 +0900

    [update] application.rbを修正し、国際化対応する

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,11 @@ module InstaClone
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # 国際化対応
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
