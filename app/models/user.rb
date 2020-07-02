39dcfea5b664de2fe14b88d51e353b85c2ecdbeb
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-02 13:54:10 +0900

    [update] bundle exec annotate （Userモデル）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  crypted_password :string(255)
+#  email            :string(255)      not null
+#  salt             :string(255)
+#  username         :string(255)      not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
