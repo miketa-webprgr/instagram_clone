70bd795b5ec12ab9a00dcf40e88d433fc709991d
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-08 08:33:06 +0900

    [fix] user_sessions_controller.rbのコメントを修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -11,8 +11,7 @@ class UserSessionsController < ApplicationController
 
     if @user
       # sorceryの独自メソッド
-      # ログインしている時はroot_pathへ、ログアウトしている時はログインページへアクセスさせる？
-      # 公式ドキュメント等を見たけれども、いまいち何が言いたいのかよく分からない
+      # 未ログイン状態でログインが必要な画面に遷移しようとする → ログインしてないのでログイン画面に遷移する → ログインする → root_pathではなく当初遷移しようとしていたページに遷移する
       redirect_back_or_to root_path, success: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
