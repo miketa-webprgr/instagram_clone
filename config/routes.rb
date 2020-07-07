80c460a5ac857956d09b335af510b4fa11310912
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-07 18:34:35 +0900

    [add] 投稿のCRUD機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root 'posts#index'
 
   resources :users, only: %i[new create]
+  resources :posts
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
