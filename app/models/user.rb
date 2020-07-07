a5de2acb812255e67dcb0ccbdf656d1b88197d4d
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-07 19:12:31 +0900

    [fix] ログインした場合、権限を持つ投稿に対して編集・削除ができるアイコンを表示する
    index.html.slim(_post.html.slim）及びshow.html.slimにてロジックは実装済

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,10 @@ class User < ApplicationRecord
   # [Active Record の関連付け \- Railsガイド](https://railsguides.jp/association_basics.html#belongs-to%E3%81%AE%E3%82%AA%E3%83%97%E3%82%B7%E3%83%A7%E3%83%B3-dependent)
   has_many :posts, dependent: :destroy
 
+  # objectには@postを代入する
+  # 一覧表示されている投稿のidが、current_user.user_idと一致しているか確認する
+  # 一致していれば、編集と削除のアイコンを表示させる（index.html.slim及びshow.html.slimにて）
+  def own?(object)
+    id == object.user_id
+  end
 end
