e3eae39ad65f2cab02474ec53a71b4d74dde8f2e
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-07 18:33:45 +0900

    [fix] post.rbにserializeメソッドを追加（複数の画像を扱うため）

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,10 +19,10 @@
 #
 class Post < ApplicationRecord
   mount_uploaders :images, ImageUploader
-  # Rails5.0未満を使ってる場合は以下のコードも必要とのこと
-  # なので、試しにコメントアウトしていても問題がないか実験してみる
-  # serialize :images, JSON
-  
+  # 複数の画像を取り扱う場合、serializeメソッドが必要
+  # JSON形式でなくとも、複数の画像を受け取ることは可能
+  # ただ、posts_controllerにてJSON形式でデータを受け取るよう指定しているので、整合性を取る必要あり
+  serialize :images, JSON
   validates :body, presence: true, length: { maximum: 1000 }
   validates :images, presence: true  
 
