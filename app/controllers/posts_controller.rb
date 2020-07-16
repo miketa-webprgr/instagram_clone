451d7ee5a1bc020c2becb620399b8e98056f49f4
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-16 16:29:16 +0900

    [add] コメント一覧を表示させる
    show, _comments, _commentのhtml.slimファイルとpostsコントローラの追記・修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,6 +44,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    # N+1 問題については、あとで考える
+    @comments = @post.comments.order(created_at: :desc)
   end
 
   def destroy
