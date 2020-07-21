48d07c2d64e46522325d1fb1007dad555f67a312
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-21 22:42:16 +0900

    [note] 疑問点をコメントに追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,6 +5,8 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    # @post = Post.find(params[:post_id])とした方が分かりやすい気がするが、この書き方はNGか？(createアクションと書き方が似るため)
+    # その場合、unlike.html.slimの方でparamsを送る形にする
     @post = Like.find(params[:id]).post
     current_user.unlike(@post)
   end
