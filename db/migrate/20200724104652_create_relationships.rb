8ea3b4a1e99bfb775200eb93923cbd922ab2b070
miketa_webprgr <ksuedomi91@gmail.com>
2020-07-24 19:58:40 +0900

    [add] マイグレーションファイルにindexを付与し、ユニーク制約を追加

--- db/migrate/20200724104652_create_relationships.rb
+++ db/migrate/20200724104652_create_relationships.rb
@@ -6,5 +6,11 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
+    # フォローするユーザー・フォローされるユーザーのidについて、indexを貼る
+    # これは何のために必要なのか？ 検索を早くするため？
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    # フォローやアンフォローができないよう、ユニーク制約をつける
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
