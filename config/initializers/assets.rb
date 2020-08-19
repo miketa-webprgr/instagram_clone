44e637dc8cff978bbf83a108f54c82430bc7ca02
miketa_webprgr <ksuedomi91@gmail.com>
2020-08-19 20:11:32 +0900

    [clean] rubocopによるチェック

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # なお、英語で書かれているとおり、application.jsやcssは書かなくてもprecompileされている
-Rails.application.config.assets.precompile += %w( mypage.js mypage.css )
+Rails.application.config.assets.precompile += %w[mypage.js mypage.css]
