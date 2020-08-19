ab3da893b5fe465043de19eecd772ac77f93ff06
miketa_webprgr <ksuedomi91@gmail.com>
2020-08-19 15:38:03 +0900

    [add] rails g migration AddAvatarToUsers avatar:string
    db:migrateも実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id               :bigint           not null, primary key
+#  avatar           :string(255)
 #  crypted_password :string(255)
 #  email            :string(255)      not null
 #  salt             :string(255)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_104652) do
+ActiveRecord::Schema.define(version: 2020_08_19_063515) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_07_24_104652) do
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
