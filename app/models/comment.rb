e093085fb28debde534d4ee08bed2958b9db0613
miketa_webprgr <ksuedomi91@gmail.com>
2020-09-03 20:24:44 +0900

    [update] comments_controllerにあるNGワードのロジックをモデルに寄せた
    本来であればカスタムバリデータで対応したいところではあるが、とりあえずこれで対応

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -27,4 +27,13 @@ class Comment < ApplicationRecord
 
   # NULL制約と文字列長1000文字の制約を追加
   validates :body, presence: true, length: { maximum: 1000 }
+
+  # NGワードが含まれてるかチェックし、true/falseを返すメソッド
+  def has_ng_words?
+    # NGワードをここで読み込む
+    ng_word = Swearjar.new('config/locales/my_swears.yml')
+    # NGワードを含んでいるとtrueを返す
+    ng_word.profane?(self.body)
+  end
+
 end
