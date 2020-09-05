1f7142081dcbade3d8bb12361807b18e79eb1ac9
miketa_webprgr <ksuedomi91@gmail.com>
2020-09-05 11:15:09 +0900

    [fix] reads_controller.rbのcurrent_userの漏れを修正(詳細参照）
    @notification = current_user.notifications.find~とした

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -2,7 +2,7 @@ class ReadsController < ApplicationController
   before_action :require_login, only: %i[create]
 
   def create
-    @notification = Notification.find(params[:notification_id])
+    @notification = current_user.notifications.find(params[:notification_id])
     @notification.read! if @notification.unread?
     redirect_to @notification.appropiate_path
   end
