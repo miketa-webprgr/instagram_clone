01dd1fe3c1808c16e04da22f009a90e0507d6b55
miketa_webprgr <ksuedomi91@gmail.com>
2020-09-12 15:08:02 +0900

    [clean] rubocopによるチェック

--- config/routes.rb
+++ config/routes.rb
@@ -33,5 +33,5 @@ Rails.application.routes.draw do
   end
 
   # LetterOpenerWebのルーティングを設定する
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
