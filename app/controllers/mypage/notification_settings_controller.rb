f336038528a1cbd6d4ba6b826a02955c287fbcf9
miketa_webprgr <ksuedomi91@gmail.com>
2020-10-03 21:49:59 +0900

    [add] notification_settings_controllerを実装

--- app/controllers/mypage/notification_settings_controller.rb
+++ app/controllers/mypage/notification_settings_controller.rb
@@ -4,5 +4,18 @@ class Mypage::NotificationSettingsController < Mypage::BaseController
   end
 
   def update
+    @user = User.find(current_user.id)
+    if @user.update(notification_settings_params)
+      redirect_to edit_mypage_notification_setting_path, success: 'メール通知設定を更新しました'
+    else
+      flash.now['danger'] = 'メール通知設定の更新に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def notification_settings_params
+    params.require(:user).permit(:notification_on_comment, :notification_on_like, :notification_on_follow)
   end
 end
